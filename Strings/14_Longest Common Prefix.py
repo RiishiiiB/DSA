23b3edddd095d33f7a3a640e3efd5b816a60eac5
MinatoUzu <rishi17org@gmail.com>
2026-09-04 22:28:36 +0530

    14.Solved

---
+++ Strings/14_Longest Common Prefix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        prefix = strs[0]
+        for word in strs:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+        return prefix
